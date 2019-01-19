42a92816edafa5a6bc0a8a8292e3ff5018913946
liangwei0101 <liangwei0101@outlook.com>
2019-01-19 18:52:50 +0800

    增加文件

--- vue-hello-world/src/main.js
+++ vue-hello-world/src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
+Vue.use(iView)
 Vue.config.productionTip = false
 
 new Vue({
--- vue-hello-world/src/views/About.vue
+++ vue-hello-world/src/views/About.vue
@@ -1,5 +1,93 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="test">
+    <div type="flex" class="right" justify="end">
+         <Row type="flex" justify="end" class="code-row-bg">
+                 <Button type="info">Info</Button>
+    </Row>
+
+    </div>
+    <div>
+      <Table border :columns="columns12" :data="data6">
+        <template slot-scope="{ row }" slot="name">
+          <strong>{{ row.name }}</strong>
+        </template>
+        <template slot-scope="{ row, index }" slot="action">
+          <Button type="primary" size="small" style="margin-right: 5px" @click="show(index)">View</Button>
+          <Button type="error" size="small" @click="remove(index)">Delete</Button>
+        </template>
+      </Table>
+    </div>
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        columns12: [{
+            title: "Name",
+            slot: "name"
+          },
+          {
+            title: "Age",
+            key: "age"
+          },
+          {
+            title: "Address",
+            key: "address"
+          },
+          {
+            title: "Action",
+            slot: "action",
+            width: 150,
+            align: "center"
+          }
+        ],
+        data6: [{
+            name: "John Brown",
+            age: 18,
+            address: "New York No. 1 Lake Park"
+          },
+          {
+            name: "Jim Green",
+            age: 24,
+            address: "London No. 1 Lake Park"
+          },
+          {
+            name: "Joe Black",
+            age: 30,
+            address: "Sydney No. 1 Lake Park"
+          },
+          {
+            name: "Jon Snow",
+            age: 26,
+            address: "Ottawa No. 2 Lake Park"
+          }
+        ]
+      };
+    },
+    methods: {
+      show(index) {
+        this.$Modal.info({
+          title: "User Info",
+          content: `Name：${this.data6[index].name}<br>Age：${
+          this.data6[index].age
+        }<br>Address：${this.data6[index].address}`
+        });
+      },
+      remove(index) {
+        this.data6.splice(index, 1);
+      }
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .test {
+    margin: 20px;
+  }
+
+  .right {
+    margin:15px;
+  }
+</style>
