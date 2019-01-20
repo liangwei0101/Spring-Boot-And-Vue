cf617874d901ade86931fa1d17a02d02eee9cb4e
liangwei0101 <liangwei0101@outlook.com>
2019-01-20 16:22:15 +0800

    增加对话框

--- vue-hello-world/src/views/About.vue
+++ vue-hello-world/src/views/About.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="test">
-    <div type="flex" class="right" justify="end">
-         <Row type="flex" justify="end" class="code-row-bg">
-                 <Button type="info">Info</Button>
-    </Row>
 
+    <div type="flex" class="right" justify="end">
+      <Row type="flex" justify="end" class="code-row-bg">
+        <Button type="info" @click="AddBtnShow">新增</Button>
+      </Row>
     </div>
+
     <div>
       <Table border :columns="columns12" :data="data6">
         <template slot-scope="{ row }" slot="name">
@@ -17,13 +18,58 @@
         </template>
       </Table>
     </div>
+
+    <div>
+      <Modal v-model="isAddShow" draggable footer-hide scrollable title="增加用户">
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+          <FormItem label="姓名" prop="name">
+            <Input v-model="formValidate.name" placeholder="Enter your name"></Input>
+          </FormItem>
+          <FormItem label="年龄" prop="mail">
+            <Input v-model="formValidate.mail" placeholder="Enter your e-mail" />
+          </FormItem>
+          <FormItem label="地址" prop="mail">
+            <Input v-model="formValidate.mail" placeholder="Enter your e-mail" />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" @click="handleSubmit('formValidate')">Submit</Button>
+            <Button @click="handleReset('formValidate')" style="margin-left: 8px">Reset</Button>
+          </FormItem>
+        </Form>
+      </Modal>
+    </div>
+
+    <div>
+      <Modal v-model="isEditShow" draggable footer-hide scrollable title="修改用户">
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+          <FormItem label="工号" prop="name">
+            <Input v-model="formValidate.name" placeholder="Enter your name" />
+          </FormItem>
+          <FormItem label="姓名" prop="mail">
+            <Input v-model="formValidate.mail" placeholder="Enter your e-mail" />
+          </FormItem>
+          <FormItem label="邮箱" prop="mail">
+            <Input v-model="formValidate.mail" placeholder="Enter your e-mail" />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" @click="handleSubmit('formValidate')">Submit</Button>
+            <Button @click="handleReset('formValidate')" style="margin-left: 8px">Reset</Button>
+          </FormItem>
+        </Form>
+      </Modal>
+    </div>
+
   </div>
 </template>
 <script>
-  import {getMessage} from '../api/user.js'
+  import {
+    getMessage
+  } from "../api/user.js"
   export default {
     data() {
       return {
+        isAddShow: false,
+        isEditShow: false,
         columns12: [{
             title: "Name",
             slot: "name"
@@ -63,7 +109,30 @@
             age: 26,
             address: "Ottawa No. 2 Lake Park"
           }
-        ]
+        ],
+        formValidate: {
+          name: "",
+          mail: "",
+          city: ""
+        },
+        ruleValidate: {
+          name: [{
+            required: true,
+            message: "The name cannot be empty",
+            trigger: "blur"
+          }],
+          mail: [{
+              required: true,
+              message: "Mailbox cannot be empty",
+              trigger: "blur"
+            },
+            {
+              type: "email",
+              message: "Incorrect email format",
+              trigger: "blur"
+            }
+          ]
+        }
       };
     },
     methods: {
@@ -77,10 +146,17 @@
       },
       remove(index) {
         this.data6.splice(index, 1);
+      },
+      AddBtnShow() {
+        this.handleReset('formValidate')
+        this.isAddShow = true
+      },
+      handleReset (name) {
+        this.$refs[name].resetFields();
       }
     },
     mounted() {
-      getMessage()
+      getMessage();
     }
   };
 </script>
@@ -92,6 +168,6 @@
   }
 
   .right {
-    margin:15px;
+    margin: 15px;
   }
 </style>
