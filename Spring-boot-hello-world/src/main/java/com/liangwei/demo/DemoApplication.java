13bf4bbc32debb9034a1427fd86ef90b8634af20
liangwei0101 <liangwei0101@outlook.com>
2019-01-30 11:24:24 +0800

    将打包成war包的配置去除

--- Spring-boot-hello-world/src/main/java/com/liangwei/demo/DemoApplication.java
+++ Spring-boot-hello-world/src/main/java/com/liangwei/demo/DemoApplication.java
@@ -7,16 +7,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(DemoApplication.class);
-	}
-
 }
 
